d72c9cb66bb86222403ee67957864e9ae5df3651
Ryan Walters <shout at ryanwalters dot co>
2020-06-22 15:44:33 -0500

    refactor: use screen instead of destructuring render; don't use cleanup

--- src/templates/component/componentTestTestingLibraryTemplate.js
+++ src/templates/component/componentTestTestingLibraryTemplate.js
@@ -1,14 +1,13 @@
 module.exports = `import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import TemplateName from './TemplateName';
 
 describe('<TemplateName />', () => {
-  afterEach(cleanup);
-
   test('it should mount', () => {
-    const { getByTestId } = render(<TemplateName />);
-    const templateName = getByTestId('TemplateName');
+    render(<TemplateName />);
+    
+    const templateName = screen.getByTestId('TemplateName');
 
     expect(templateName).toBeInTheDocument();
   });
