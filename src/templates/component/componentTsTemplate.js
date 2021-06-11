a1f25ec4d0df8523b614329a4faa177a8287bbb6
laurens <laurens.inauen@hkb.bfh.ch>
2021-06-11 16:53:52 +0200

    change TypeScript template to not use React.FC anymore since it is not recommended anymore.

--- src/templates/component/componentTsTemplate.js
+++ src/templates/component/componentTsTemplate.js
@@ -1,7 +1,7 @@
 module.exports = `import React from 'react';
 import styles from './TemplateName.module.css';
 
-const TemplateName: React.FC = () => (
+const TemplateName = () => (
   <div className={styles.TemplateName} data-testid="TemplateName">
     TemplateName Component
   </div>
