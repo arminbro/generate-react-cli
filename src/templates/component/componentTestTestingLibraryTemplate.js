99b5756a680c2faf66f58d4e5eed2109328125ba
Armin Broubakarian <armin.broubakarian@gmail.com>
2024-05-18 11:43:25 -0500

    fix: default TestingLibraryTemplate to drop 'extend-expect' from the @testing-library import path

--- src/templates/component/componentTestTestingLibraryTemplate.js
+++ src/templates/component/componentTestTestingLibraryTemplate.js
@@ -1,12 +1,12 @@
 export default `import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import TemplateName from './TemplateName';
 
 describe('<TemplateName />', () => {
   test('it should mount', () => {
     render(<TemplateName />);
-    
+
     const templateName = screen.getByTestId('TemplateName');
 
     expect(templateName).toBeInTheDocument();
